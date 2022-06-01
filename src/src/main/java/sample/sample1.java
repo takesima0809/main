c076f6645154e8561c0ba318ee34ee839e684765
takeshima0809 <h.takeshima@ksjinc.onmicrosoft.com>
2022-06-01 11:12:32 +0900

    aaa コメント

---
+++ src/src/main/java/sample/sample1.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("aaaa");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/test/gitTest.java
+++ src/test/gitTest.java
@@ -3,5 +3,5 @@ package test;
 public class gitTest {
 	System.out.print("hello");
 	System.out.print("hi");
-	
-}
+	}
+

2025cb05ef20905dbb2f949b869928f666677c97
koya0627 <k.nagasaki@ac.ksj.co.jp>
2022-06-02 13:13:35 +0900

    valuesパッケージ追加、TotalPriceクラス作成

--- チーム制作フォルダ/src/main/java/clothesValues/WaitDay.java
+++ チーム制作フォルダ/src/main/java/clothesValues/WaitDay.java
@@ -1,5 +1,13 @@
 package clothesValues;
 
 public class WaitDay {
-
+	private final int waitDay;
+	
+	public WaitDay(int waitDay) {
+		this.waitDay=waitDay;
+	}
+	
+	public int toInt() {
+		return this.waitDay;
+	}
 }

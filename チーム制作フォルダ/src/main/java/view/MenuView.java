ac7660114d6373248b3c516c21244eb25e9b0c30
kagawa8091 <y.kagawa@ac.ksj.co.jp>
2022-06-02 13:42:05 +0900

    viewいじりました

--- チーム制作フォルダ/src/main/java/view/MenuView.java
+++ チーム制作フォルダ/src/main/java/view/MenuView.java
@@ -1,5 +1,23 @@
 package view;
 
-public class MenuView {
+import java.util.Scanner;
 
+public class MenuView {
+//メニュー画面
+	public void viewMenu()
+	{
+		System.out.println("行う業務を選択してください");
+		Scanner scan = new Scanner(System.in);
+		String scanS=scan.next();
+
+	}
+	
+	public void viewShop()
+	{
+	}
+	
+	public void viewFactory()
+	{
+		
+	}
 }

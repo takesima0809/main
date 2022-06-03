dc7c196b782263268bb6e86b28584e90846e754c
kagawa8091 <y.kagawa@ac.ksj.co.jp>
2022-06-03 11:25:07 +0900

    view追記

--- チーム制作フォルダ/src/main/java/sample/ViewTest.java
+++ チーム制作フォルダ/src/main/java/sample/ViewTest.java
@@ -7,16 +7,8 @@ public class ViewTest {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		MenuView menu=new MenuView();
-		int a=menu.viewMenu();
+		int a=menu.selectFactoryOrShop();
 		
-		if(a==1)
-		{
-			menu.viewShop();
-		}
-		else if(a==2)
-		{
-			menu.viewFactory();
-		}
 	}
 
 }

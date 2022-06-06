5aed56c149c1a9d3c03c84797e6ec0eda2c9d824
takeshima0809 <h.takeshima@ksjinc.onmicrosoft.com>
2022-06-06 10:19:30 +0900

    サービス追加

--- チーム制作フォルダ/src/main/java/service/UserService.java
+++ チーム制作フォルダ/src/main/java/service/UserService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class UserService {
+import entities.DepositDataList;
+import repositories.ShopRepository;
 
+public class UserService {
+	ShopRepository shopRepository;
+	public UserService() {
+		this.shopRepository =new ShopRepository();
+	}
+	public DepositDataList UserFindAll() {
+		return null;
+	}
+	public DepositDataList UserAdd() {
+		return null;
+	}
 }

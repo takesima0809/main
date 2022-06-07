e4d3b7277084b3735e7525bfdf3d9353fc1dd0fd
takeshima0809 <h.takeshima@ksjinc.onmicrosoft.com>
2022-06-07 10:08:52 +0900

    updataService

--- チーム制作フォルダ/src/main/java/service/UpdataService.java
+++ チーム制作フォルダ/src/main/java/service/UpdataService.java
@@ -3,8 +3,14 @@ package service;
 import repositories.ShopRepository;
 
 public class UpdataService {
-	ShopRepository shopRepository;
-	public AddService() {
+	private final ShopRepository shopRepository;
+	public UpdataService() {
 		this.shopRepository = new ShopRepository();
+	}
+	public void updataFinishDate(int depositId,String day) {
+		this.shopRepository.UpdateDay(depositId, day);
+	}
+	public void updataMessage(int depositId,String message) {
+		this.shopRepository.addMessage(depositId, message);
 	}
 }
